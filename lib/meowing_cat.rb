1abb5fbd79b976e7a1c5842a60ceb2382476ffae
carly <carly@noukod.com>
2018-10-01 14:17:05 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+
+def initialize name
+  @name = name
+end
+
+def meow
+  puts "meow"
+end
+end
+
+maru = Cat.new
+maru.name = "Maru"
+
+maru.name
+# => "Maru"
+
+maru.meow
